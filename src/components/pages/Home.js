444654212ced36960ceca3dfa99bb6f08138f80f
ramirezandradesara <sararamirezandrade@gmail.com>
2022-05-12 13:45:39 -0300

    carousel done

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,48 +1,40 @@
 import React from 'react'
-import { Carousel, Table } from 'react-bootstrap';
+import { Carousel } from 'react-bootstrap';
+import './Home.css'
 
 function Home() {
   return (
-
+    <>
       <Carousel>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            // src="holder.js/800x400?text=First slide&bg=373940"
-            src="images/rollers1.jpg"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="images/rollers2.jpg"
-            alt="Second slide"
-          />
 
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="images/rollers3.jpg"
-            alt="Third slide"
-          />
-
+          <div className='img-container1'> </div>
           <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            <h3>TWISTER XT AVAILABLE </h3>
+            <p>THE NEWEST IN PERFORMANCE FOR FREESKATING</p>
           </Carousel.Caption>
         </Carousel.Item>
+
+        <Carousel.Item>
+        <div className='img-container2'> </div>
+          <Carousel.Caption>
+            <h3>AUTUMN 2022 COLLECTION</h3>
+            <p>CHECK OUT URBAN CLOTHING FOR THIS SEASON.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+
+        <Carousel.Item >
+        <div className='img-container3'> </div>
+          <Carousel.Caption>
+            <h3>UPGRADE TO HYDROGEN PRO</h3>
+            <p>FOR YOU SPEED WHEELS NEEDS.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+
       </Carousel>
-    
+
+      
+    </>
   )
 }
 
