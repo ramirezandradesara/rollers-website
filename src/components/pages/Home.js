075f6ce08b8fd91144bfff258041a6daf2294320
ramirezandradesara <sararamirezandrade@gmail.com>
2022-05-11 23:56:00 -0300

    carousel home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,48 @@
 import React from 'react'
+import { Carousel, Table } from 'react-bootstrap';
 
 function Home() {
   return (
-    <div>Home</div>
+
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            // src="holder.js/800x400?text=First slide&bg=373940"
+            src="images/rollers1.jpg"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="images/rollers2.jpg"
+            alt="Second slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="images/rollers3.jpg"
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    
   )
 }
 
